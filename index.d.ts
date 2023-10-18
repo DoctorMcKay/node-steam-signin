a6c100cbebd6ae7344e7d00d2fc343695e84e20a
Henrique <henriquecfreitas@hotmail.com.br>
2023-10-18 11:12:39 -0300

    Introducing module declaration file

---
+++ index.d.ts
@@ -0,0 +1,9 @@
+import SteamID from 'steamid';
+
+declare class SteamSignIn {
+  constructor(realm: string);
+  getUrl(returnUrl: string): string;
+  verifyLogin(url: string): Promise<SteamID>;
+}
+
+export default SteamSignIn;
